3a0ebdc7323281f58cf2ad2acbd5b35ea8f7d213
William Wills <wwills2@protonmail.com>
2024-10-14 13:10:42 -0400

    feat: updated activities detail modal to use updated explorer activity record resource

--- src/renderer/pages/ActivitiesPage.tsx
+++ src/renderer/pages/ActivitiesPage.tsx
@@ -1,6 +1,12 @@
 import { useColumnOrderHandler, useQueryParamState, useWildCardUrlHash } from '@/hooks';
 import { debounce } from 'lodash';
-import { ActivitiesListTable, ActivityDetailsModal, SearchBox, SkeletonTable } from '@/components';
+import {
+  ActivitiesListTable,
+  ActivityDetailsModal,
+  IndeterminateProgressOverlay,
+  SearchBox,
+  SkeletonTable,
+} from '@/components';
 import { FormattedMessage } from 'react-intl';
 import { useGetActivitiesQuery } from '@/api';
 import { RECORDS_PER_PAGE } from '@/api/climate-explorer/v1';
@@ -26,6 +32,7 @@ const ActivitiesPage: React.FC = () => {
   const {
     data: activitiesData,
     isLoading: activitiesQueryLoading,
+    isFetching: activitiesQueryFetching,
     error: activitiesQueryError,
   } = useGetActivitiesQuery({
     search,
@@ -35,6 +42,14 @@ const ActivitiesPage: React.FC = () => {
 
   const handleSearchChange = debounce((event: any) => setSearch(event.target.value), 800);
   const handlePageChange = debounce((page) => setCurrentPage(page), 800);
+  const handleActivitiesTableRowClick = (activity: Activity) => {
+    const warehouseUnitId = activity?.cw_unit?.warehouseUnitId || '';
+    const coinId = activity?.coin_id || '';
+    const actionMode = activity?.mode || '';
+    const urlHashContents: string = warehouseUnitId + '^' + coinId + '^' + actionMode;
+
+    setShowActivityDetailsModalActive(true, urlHashContents);
+  };
 
   if (activitiesQueryError) {
     return <FormattedMessage id={'unable-to-load-contents'} />;
@@ -56,6 +71,7 @@ const ActivitiesPage: React.FC = () => {
 
   return (
     <>
+      {activitiesQueryFetching && <IndeterminateProgressOverlay />}
       <div className="pt-2 pl-2 pr-2 h-full">
         <div className="flex flex-col md:flex-row gap-6 my-2.5 relative z-30 items-center h-auto">
           <SearchBox defaultValue={search} onChange={handleSearchChange} />
@@ -66,14 +82,13 @@ const ActivitiesPage: React.FC = () => {
           currentPage={Number(currentPage)}
           onPageChange={handlePageChange}
           setOrder={handleSetOrder}
-          onRowClick={(activity: Activity) =>
-            setShowActivityDetailsModalActive(true, activity?.cw_unit?.warehouseUnitId || '')
-          }
+          onRowClick={handleActivitiesTableRowClick}
           order={order}
           totalPages={Math.ceil(activitiesData.total / RECORDS_PER_PAGE)}
           totalCount={activitiesData.total}
         />
       </div>
+
       {showActivityDetailsModal && (
         <ActivityDetailsModal
           warehouseUnitId={activityDetailsModalUrlFragment.replace('activity-details-', '')}
